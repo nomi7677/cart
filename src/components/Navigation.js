a22bb5608162cd6e067774d5b9381fc9a7f87b35
nomi7677 <nomi7677@yahoo.com>
2021-08-11 12:19:29 -0400

    cart

---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+
+import {Link} from 'react-router-dom';
+
+const Navigation = () => {
+    return (
+        <>
+
+            <Link to='/'>Home</Link>
+            <Link to='/about'>About</Link>
+        </>
+    )
+}
+
+export default Navigation;
